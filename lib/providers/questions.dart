6544ff57825572785959af50feb82382d6ba91ed
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-11 03:13:42 +0900

    Question用のProviderを作成しました

---
+++ lib/providers/questions.dart
@@ -0,0 +1,13 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/models/services/question_service.dart';
+
+
+final _questionServiceProvider = Provider((ref) => FirebaseQuestionService(ref.read));
+
+class QuestionProviders {
+  QuestionProviders._();
+
+  static Provider<QuestionService> questionServiceProvider() {
+    return _questionServiceProvider;
+  }
+}
