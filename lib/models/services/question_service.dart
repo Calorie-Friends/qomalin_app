9728eec48ee83d87b19224eac5f5004a2dea1bb2
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-11 02:41:47 +0900

    質問を取得するためのServiceを作成しました

--- lib/models/services/question_service.dart
+++ lib/models/services/question_service.dart
@@ -1,10 +1,37 @@
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:qomalin_app/models/entities/question.dart';
+import 'package:qomalin_app/providers/firestore.dart';
+
 
 abstract class QuestionService {
 
+  Stream<List<Question>> nearQuestions({required double radius, required double latitude, required double longitude});
+
+  Stream<List<Question>> latestQuestions({int limit = 20});
 
 }
 
 class FirebaseQuestionService extends QuestionService {
+  final Reader reader;
+  FirebaseQuestionService(this.reader);
+  @override
+  Stream<List<Question>> nearQuestions({required double radius, required double latitude, required double longitude}) {
+    final collection = reader(firestoreProvider).collection("questions");
+    final geo = reader(geoFirestoreProvider);
+    return geo.collection(collectionRef: collection).within(
+        center: GeoFirePoint(latitude, longitude),
+        radius: radius,
+        field: "location"
+    ).map((event) => event.map((e) => Question.fromDocument(e)).toList());
+  }
 
+  @override
+  Stream<List<Question>> latestQuestions({int limit = 20}) {
+    return reader(FirestoreProviders.questionCollectionRefProvider())
+      .orderBy("created_at", descending: true)
+      .limit(limit)
+      .snapshots()
+      .map((event) => event.docs.map((e) => e.data()).toList());
+  }
 }
