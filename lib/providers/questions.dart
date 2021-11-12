1cd74eb9b13ac5bff6b98b307a7aaa4527043b90
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-12 11:52:29 +0900

    QuestionRepositoryを持ってくるProviderを作成しました。

--- lib/providers/questions.dart
+++ lib/providers/questions.dart
@@ -1,13 +1,20 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/models/repositories/question_repository.dart';
 import 'package:qomalin_app/models/services/question_service.dart';
 
 final _questionServiceProvider =
     Provider((ref) => FirebaseQuestionService(ref.read));
+
+final _questionRepositoryProvider = Provider((ref) => FirebaseQuestionRepository(ref.read));
 
 class QuestionProviders {
   QuestionProviders._();
 
   static Provider<QuestionService> questionServiceProvider() {
     return _questionServiceProvider;
+  }
+
+  static Provider<QuestionRepository> questionRepositoryProvider() {
+    return _questionRepositoryProvider;
   }
 }
