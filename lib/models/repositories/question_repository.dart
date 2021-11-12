4625505f3712e7df12950f02e768b042b598e662
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-12 11:52:14 +0900

    QuestionRepositoryを作成しました。

---
+++ lib/models/repositories/question_repository.dart
@@ -0,0 +1,20 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/models/entities/question.dart';
+import 'package:qomalin_app/providers/firestore.dart';
+
+abstract class QuestionRepository {
+  Future<Question?> create(Question question);
+}
+
+class FirebaseQuestionRepository extends QuestionRepository{
+  Reader reader;
+  FirebaseQuestionRepository(this.reader);
+  @override
+  Future<Question?> create(Question question) async {
+    final addResult = await reader(FirestoreProviders.questionCollectionRefProvider())
+      .add(question);
+    final res = await addResult.get();
+    return res.data();
+
+  }
+}
