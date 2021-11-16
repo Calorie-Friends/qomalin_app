3b650060c80a2f5c9d08d736a855722ef18ea1b6
tak2355 <chums.tak6@gmail.com>
2021-11-16 16:07:55 +0900

    変数名を修正した。

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         body: ListView(
       padding: const EdgeInsets.all(16),
-      children: [QuestionCard(title: '自販機が使えないです。',text: '自販機が使えないので助けてください！',avataricon: 'https://avatars.githubusercontent.com/u/65577595?v=4',username: 'tak2355',onQuestionPressed: onQuestionPressed,onUserPressed: onUserPressed,)],
+      children: [QuestionCard(title: '自販機が使えないです。',text: '自販機が使えないので助けてください！',avatarIcon: 'https://avatars.githubusercontent.com/u/65577595?v=4',username: 'tak2355',onQuestionPressed: onQuestionPressed,onUserPressed: onUserPressed,)],
     ));
   }
 }
@@ -25,14 +25,14 @@ class HomePage extends StatelessWidget {
 class QuestionCard extends StatelessWidget {
   final String title;
   final String text;
-  final String? avataricon;
+  final String? avatarIcon;
   final String username;
   final VoidCallback onQuestionPressed;
   final VoidCallback onUserPressed;
   const QuestionCard({
     Key?key,required this.title,
     required this.text,
-    required this.avataricon,
+    required this.avatarIcon,
     required this.username,
     required this.onQuestionPressed,
     required this.onUserPressed
@@ -67,7 +67,7 @@ class QuestionCard extends StatelessWidget {
                     borderRadius: BorderRadius.circular(20),
                     onTap: onUserPressed,
                     child: CircleAvatar(
-                      foregroundImage: avataricon == null ? const NetworkImage('https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1703/tuktukdesign170300061/73583439-%E7%94%B7%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC-%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3-%E3%83%97%E3%83%AD%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB-%E3%82%A2%E3%83%90%E3%82%BF%E3%83%BC-%E3%82%B0%E3%83%AA%E3%83%95-%E3%83%99%E3%82%AF%E3%83%88%E3%83%AB-%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88.jpg?ver=6') : NetworkImage(avataricon!),
+                      foregroundImage: avatarIcon == null ? const NetworkImage('https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1703/tuktukdesign170300061/73583439-%E7%94%B7%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC-%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3-%E3%83%97%E3%83%AD%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB-%E3%82%A2%E3%83%90%E3%82%BF%E3%83%BC-%E3%82%B0%E3%83%AA%E3%83%95-%E3%83%99%E3%82%AF%E3%83%88%E3%83%AB-%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88.jpg?ver=6') : NetworkImage(avatarIcon!),
                       backgroundImage: const NetworkImage('https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1703/tuktukdesign170300061/73583439-%E7%94%B7%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC-%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3-%E3%83%97%E3%83%AD%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB-%E3%82%A2%E3%83%90%E3%82%BF%E3%83%BC-%E3%82%B0%E3%83%AA%E3%83%95-%E3%83%99%E3%82%AF%E3%83%88%E3%83%AB-%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88.jpg?ver=6'),
                       radius: 20,
                     ),
