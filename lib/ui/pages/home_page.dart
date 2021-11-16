f8ca0b47b701e2878bfd5733f7925f8f3ec69af6
tak2355 <chums.tak6@gmail.com>
2021-11-16 14:05:43 +0900

    QuestionCardのフィールドを定義した。

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -3,19 +3,40 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+  void onQuestionPressed(){
 
+  }
+  void onUserPressed(){
+
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         body: ListView(
       padding: EdgeInsets.all(16),
-      children: [QuestionCard()],
+      children: [QuestionCard(title: 'aaa',text: 'bbb',avataricon: 'ccc',username: 'ddd',onQuestionPressed: onQuestionPressed,onUserPressed: onUserPressed,)],
     ));
   }
 }
 
 class QuestionCard extends StatelessWidget {
   @override
+
+  final String title;
+  final String text;
+  final String? avataricon;
+  final String username;
+  final VoidCallback onQuestionPressed;
+  final VoidCallback onUserPressed;
+  QuestionCard({
+    Key?key,required this.title,
+    required this.text,
+    required this.avataricon,
+    required this.username,
+    required this.onQuestionPressed,
+    required this.onUserPressed
+  }):super(key:key);
+
   Widget build(BuildContext context) {
     return Card(
       elevation: 4,
