0d81212c7bd9e977e56ce6bfdd76a9cb831a0ebd
tak2355 <chums.tak6@gmail.com>
2021-11-16 15:03:54 +0900

    avatariconがnullのときにデフォルトのアイコンが出るようにした。

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatelessWidget {
     return new Scaffold(
         body: ListView(
       padding: EdgeInsets.all(16),
-      children: [QuestionCard(title: 'aaa',text: 'bbb',avataricon: 'ccc',username: 'ddd',onQuestionPressed: onQuestionPressed,onUserPressed: onUserPressed,)],
+      children: [QuestionCard(title: 'aaa',text: 'bbb',avataricon: null,username: 'ddd',onQuestionPressed: onQuestionPressed,onUserPressed: onUserPressed,)],
     ));
   }
 }
@@ -65,8 +65,7 @@ class QuestionCard extends StatelessWidget {
                   new InkWell(
                     onTap: onUserPressed,
                     child: CircleAvatar(
-                      backgroundImage: NetworkImage(avataricon!),
-
+                      backgroundImage: avataricon == null ? NetworkImage('https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1703/tuktukdesign170300061/73583439-%E7%94%B7%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC-%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3-%E3%83%97%E3%83%AD%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB-%E3%82%A2%E3%83%90%E3%82%BF%E3%83%BC-%E3%82%B0%E3%83%AA%E3%83%95-%E3%83%99%E3%82%AF%E3%83%88%E3%83%AB-%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88.jpg?ver=6') : NetworkImage(avataricon!),
                       radius: 12,
                     ),
                   ),
