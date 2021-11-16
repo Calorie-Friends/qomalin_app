5560b0c44cf62fa4319b21553be4f662f9f88cdc
勇貴士 <isamu.takashi_momo@ezweb.ne.jp>
2021-11-16 16:29:29 +0900

    簡易的な作成処理実装しました。

--- lib/ui/pages/question_editor_page.dart
+++ lib/ui/pages/question_editor_page.dart
@@ -1,8 +1,17 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/models/entities/question.dart';
+import 'package:qomalin_app/providers/firestore.dart';
+import 'package:qomalin_app/providers/questions.dart';
 
 class QuestionEditorPage extends ConsumerStatefulWidget{
+  const QuestionEditorPage({Key? key}) : super(key: key);
+
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() {
@@ -10,44 +19,80 @@ class QuestionEditorPage extends ConsumerStatefulWidget{
   }
 }
 
+
+
 class QuestionEditorState extends ConsumerState{
+  final _titleEditingController = TextEditingController();
+  final _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('こまりん作成'),
+        title: const Text('こまりん作成'),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             //タイトル
             Container(
-              margin: EdgeInsets.all(16),
-              child: const TextField(
-                decoration: InputDecoration(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: "タイトル入力",
                   hintText: "必須",
+                  errorText: null,
                 ),
+                controller: _titleEditingController,
+
+
               ),
             ),
             //本文
             Container(
-              margin: EdgeInsets.all(16),
-              child: const TextField(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 minLines: 5,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: "本文入力",
                   hintText: "必須",
                 ),
+                controller: _textEditingController,
               ),
             ),
+
           ],
         ),
       ),
+      persistentFooterButtons: [
+        ElevatedButton(
+          onPressed: () async {
+            //TODO: 現在のユーザが未認証状態だった場合認証画面へ遷移するようにする。
+            final uid = FirebaseAuth.instance.currentUser!.uid;
+            final title = _titleEditingController.text;
+            final text = _textEditingController.text;
+            final location = await Geolocator.getCurrentPosition();
+            log("作成準備:title:$title, text:$text, location.latitude:${location.latitude}, location.longitude:${location.longitude}");
+            final question = Question.newQuestion(
+                ref.read(FirestoreProviders.firestoreProvider()),
+                title: title,
+                text: text,
+                userId: uid,
+                latitude: location.latitude,
+                longitude: location.longitude,
+                imageUrls: []
+            );
+            final res = await ref.read(QuestionProviders.questionRepositoryProvider())
+                .create(question);
+
+          },
+          child: const Text("保存"),
+        ),
+      ],
+
     );
   }
 }
