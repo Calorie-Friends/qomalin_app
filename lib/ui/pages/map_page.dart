bd3370543e70fa0e254229fab1c2f1e5f1c1bfa6
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-17 16:18:06 +0900

    現在位置を取得してそこに遷移するようにしました。

--- lib/ui/pages/map_page.dart
+++ lib/ui/pages/map_page.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/models/entities/question.dart';
 import 'package:qomalin_app/providers/questions.dart';
 
 class MapPage extends ConsumerStatefulWidget {
@@ -18,22 +21,32 @@ class MapPage extends ConsumerStatefulWidget {
 class MapState extends ConsumerState {
   final Completer<GoogleMapController> _controller = Completer();
 
-  CameraPosition? cameraPosition;
+  CameraPosition? _cameraPosition;
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(35.6769883, 139.7588499),
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+
+  @override
+  void didChangeDependencies() async {
+    final GoogleMapController controller = await _controller.future;
+    Geolocator.getCurrentPosition().then((value) {
+      controller.moveCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(value.latitude, value.longitude),
+            zoom: _cameraPosition?.zoom ?? 14
+          )
+        )
+      );
+    });
+    super.didChangeDependencies();
+  }
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(QuestionProviders.questionMapNotifier());
-    _controller.future.then((value){
-
+    _controller.future.then((value) {
     });
     return Scaffold(
       body: GoogleMap(
@@ -43,19 +56,18 @@ class MapState extends ConsumerState {
           _controller.complete(controller);
         },
         markers: state.questions
-            .map((e) =>
+          .map((e) =>
             Marker(
               markerId: MarkerId(e.id),
               position: LatLng(e.location.latitude, e.location.longitude),
               onTap: () {
-
+                onQuestionMarkerTapped(e);
               }
-            )
+          )
         ).toSet(),
         onCameraMove: (position) {
-          //log("カメラが動いた position:$position");
           // NOTE: 現在のカメラの位置を記録したいだけなのでsetStateなどは呼び出さない。
-          cameraPosition = position;
+          _cameraPosition = position;
         },
         onLongPress: (latLng) {
 
@@ -64,29 +76,24 @@ class MapState extends ConsumerState {
           final ct = await _controller.future;
           final visibleRegion = await ct.getVisibleRegion();
 
-          final pos = cameraPosition;
-          if(pos == null) {
+          final pos = _cameraPosition;
+          if (pos == null) {
             return;
           }
-          ref.read(QuestionProviders.questionMapNotifier().notifier).fetch(
-            LatLng(pos.target.latitude, pos.target.longitude),
-            visibleRegion
+          ref.read(QuestionProviders
+              .questionMapNotifier()
+              .notifier).fetch(
+              LatLng(pos.target.latitude, pos.target.longitude),
+              visibleRegion
           );
-
         },
-
-
-
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
       ),
     );
   }
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+
+  Future onQuestionMarkerTapped(Question q) async {
+    log("質問が選択されました:${q.title}");
   }
 }
