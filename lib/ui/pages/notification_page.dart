ac54cbaf4e27bcacc3dd5025db2d37835abae0fa
勇貴士 <isamu.takashi_momo@ezweb.ne.jp>
2021-11-19 12:28:13 +0900

    回答作成画面を作成しました。

--- lib/ui/pages/notification_page.dart
+++ lib/ui/pages/notification_page.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('notification'),
+        child: TextButton(onPressed: (){
+          GoRouter.of(context).push("/questions/hoge/answers/create");
+        }, child: Text("test")),
       ),
     );
   }
