505e8423c247c2bdba3e8e5049eb94ab97ec5c7b
勇貴士 <isamu.takashi_momo@ezweb.ne.jp>
2021-11-26 12:27:42 +0900

    プロフィール画像を選択できるようにした。

--- lib/ui/pages/profile_editor_page.dart
+++ lib/ui/pages/profile_editor_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileEditorPage extends ConsumerStatefulWidget{
   const ProfileEditorPage({Key? key}) : super(key: key);
@@ -15,6 +18,21 @@ class ProfileEditorPage extends ConsumerStatefulWidget{
 class ProfileEditorState extends ConsumerState{
   final _usernameEditingController = TextEditingController();
   final _descriptionEditingController = TextEditingController();
+  XFile? _avatarIcon;
+  final picker = ImagePicker();
+
+  Future _getAvatarIcon() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _avatarIcon = pickedFile;
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +44,22 @@ class ProfileEditorState extends ConsumerState{
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
+
+              InkWell(
+                onTap: _getAvatarIcon,
+                child: () {
+                  if(_avatarIcon == null) {
+                    return Icon(Icons.person_sharp, size: 100,);
+                  }else{
+                    return Image.file(File(_avatarIcon!.path), width: 100, height:100, fit: BoxFit.cover,);
+                  }
+                }(),
+              ),
+              SizedBox(
+                child: Container(
+                  height: 50,
+                ),
+              ),
               TextField(
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
