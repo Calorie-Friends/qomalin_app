303f6ea0f60bf3cd75d78052f98867d38008cc6f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-12-01 12:26:47 +0900

    Questionをid指定で取得するためのfindメソッドを作成＆実装しました。

--- lib/models/repositories/question_repository.dart
+++ lib/models/repositories/question_repository.dart
@@ -1,9 +1,11 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/errors/question_error.dart';
 import 'package:qomalin_app/models/entities/question.dart';
 import 'package:qomalin_app/providers/firestore.dart';
 
 abstract class QuestionRepository {
   Future<Question?> create(Question question);
+  Future<Question> find(String id);
 }
 
 class FirebaseQuestionRepository extends QuestionRepository {
@@ -16,5 +18,17 @@ class FirebaseQuestionRepository extends QuestionRepository {
             .add(QuestionFireDTO.fromEntity(reader(FirestoreProviders.firestoreProvider()), question));
     final res = await addResult.get();
     return await res.data()?.toEntity();
+  }
+
+  @override
+  Future<Question> find(String id) async {
+    final res = await reader(FirestoreProviders.questionCollectionRefProvider())
+          .doc(id)
+          .get();
+    if(!res.exists) {
+      throw QuestionNotFoundException();
+    }
+    final dto = res.data()!;
+    return await dto.toEntity();
   }
 }
