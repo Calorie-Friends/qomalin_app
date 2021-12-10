994a62835959664b4df22dab55600e8f0909898f
勇貴士 <isamu.takashi_momo@ezweb.ne.jp>
2021-12-10 10:49:51 +0900

    userIconとusernameをfirestoreから取得してアカウントページに反映した。

--- lib/ui/pages/account_page.dart
+++ lib/ui/pages/account_page.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/providers/auth.dart';
+import 'package:qomalin_app/providers/user.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends ConsumerStatefulWidget{
   const AccountPage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _AccountPageState();
+  }
+}
+
+class _AccountPageState extends ConsumerState{
+  String username = "";
+  String? _avatarIconUrl;
+
+  @override
+  void initState() {
+    final fireUser = ref.read(authNotifierProvider).fireAuthUser;
+    final isNewAccount = fireUser?.metadata.creationTime == fireUser?.metadata.lastSignInTime;
+    if(!isNewAccount) {
+      final uid = ref.read(authNotifierProvider).fireAuthUser?.uid;
+      ref.read(UserProviders.userRepository()).find(uid!).then((value) {
+        username = value.username;
+        setState(() {
+          _avatarIconUrl = value.avatarIcon;
+        });
+      });
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +49,32 @@ class AccountPage extends StatelessWidget {
               margin: const EdgeInsets.all(16),
               height: 70,
               child: Row(
-                children:const [
+                children:[
                   //TODO: firestoreからavatarとusernameを取得してUIに反映する
-                  Icon(Icons.account_circle, size: 80,),
-                  SizedBox(
+                  Container(
+                    child: () {
+                      if(_avatarIconUrl == null) {
+                        return const Icon(Icons.person_sharp, size: 60,);
+                      }else{
+                        return Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage(_avatarIconUrl!),
+                              )
+                          ),
+
+                        );
+                      }
+                    }(),
+                  ),
+                  const SizedBox(
                     width: 30,
                   ),
-                  Text("佐藤", style: TextStyle(fontSize: 25),),
+                  Text(username, style: TextStyle(fontSize: 25),),
                 ],
               ),
             ),
