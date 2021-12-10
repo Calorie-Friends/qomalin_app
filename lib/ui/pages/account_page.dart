ca38d520aba56c67c52d2f4d428a46c444ccf99b
勇貴士 <isamu.takashi_momo@ezweb.ne.jp>
2021-12-10 11:25:48 +0900

    progressを表示した。

--- lib/ui/pages/account_page.dart
+++ lib/ui/pages/account_page.dart
@@ -2,95 +2,96 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/errors/auth_error.dart';
 import 'package:qomalin_app/providers/auth.dart';
 import 'package:qomalin_app/providers/user.dart';
 
-class AccountPage extends ConsumerStatefulWidget{
+
+final _userFutureProvider = FutureProvider.autoDispose((ref) async {
+
+  final uid = ref.read(authNotifierProvider).fireAuthUser?.uid;
+  if(uid == null) {
+    throw UnauthorizedException();
+  }
+  return await ref.read(UserProviders.userRepository())
+      .find(uid);
+});
+
+class AccountPage extends ConsumerWidget{
   const AccountPage({Key? key}) : super(key: key);
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() {
-    return _AccountPageState();
-  }
-}
-
-class _AccountPageState extends ConsumerState{
-  String username = "";
-  String? _avatarIconUrl;
 
   @override
-  void initState() {
-    final fireUser = ref.read(authNotifierProvider).fireAuthUser;
-    final isNewAccount = fireUser?.metadata.creationTime == fireUser?.metadata.lastSignInTime;
-    if(!isNewAccount) {
-      final uid = ref.read(authNotifierProvider).fireAuthUser?.uid;
-      ref.read(UserProviders.userRepository()).find(uid!).then((value) {
-        username = value.username;
-        setState(() {
-          _avatarIconUrl = value.avatarIcon;
-        });
-      });
-    }
-    super.initState();
-  }
+  Widget build(BuildContext context, ref) {
+    final body = ref.watch(_userFutureProvider).map(
+      data: (data) {
+        return Column(
+          children: [
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: Container(
+                width: double.infinity,
+                margin: const EdgeInsets.all(16),
+                height: 70,
+                child: Row(
+                  children:[
+                    Container(
+                      child: () {
+                        if(data.value.avatarIcon == null) {
+                          return const Icon(Icons.person_sharp, size: 60,);
+                        }else{
+                          return Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage(data.value.avatarIcon!),
+                                )
+                            ),
 
-  @override
-  Widget build(BuildContext context) {
+                          );
+                        }
+                      }(),
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    Text(data.value.username, style: const TextStyle(fontSize: 25),),
+                  ],
+                ),
+              ),
+            ),
+            TextButton(
+                onPressed: (){
+                  GoRouter.of(context).push("/me/profile/edit");
+                },
+                child: const Text("プロフィール編集", style: TextStyle(color: Colors.black))
+            ),
+            TextButton(
+              onPressed: (){
+                //TODO: ログアウトを実装する
+              },
+              child: const Text("ログアウト"),
+            ),
+          ],
+        );
+      },
+      error: (err) {
+        return const Text("取得に失敗しました。");
+      },
+      loading: (e) {
+         return const Center(
+           child: CircularProgressIndicator(),
+         );
+      }
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text("アカウント"),
       ),
-      body: Column(
-        children: [
-          Card(
-            margin: const EdgeInsets.all(20),
-            child: Container(
-              width: double.infinity,
-              margin: const EdgeInsets.all(16),
-              height: 70,
-              child: Row(
-                children:[
-                  //TODO: firestoreからavatarとusernameを取得してUIに反映する
-                  Container(
-                    child: () {
-                      if(_avatarIconUrl == null) {
-                        return const Icon(Icons.person_sharp, size: 60,);
-                      }else{
-                        return Container(
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: NetworkImage(_avatarIconUrl!),
-                              )
-                          ),
-
-                        );
-                      }
-                    }(),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  Text(username, style: TextStyle(fontSize: 25),),
-                ],
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: (){
-              GoRouter.of(context).push("/me/profile/edit");
-            },
-            child: const Text("プロフィール編集", style: TextStyle(color: Colors.black))
-          ),
-          TextButton(
-            onPressed: (){},
-            child: const Text("ログアウト"),
-          ),
-        ],
-      ),
+      body: body
     );
   }
 }
--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -29,7 +29,7 @@ class MainState extends ConsumerState {
     HomePage(),
     const MapPage(),
     NotificationPage(),
-    const AccountPage()
+    AccountPage()
   ];
 
   @override
