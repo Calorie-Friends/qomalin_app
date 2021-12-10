e99262705d281d68bd434bdef25f32df13bd0387
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-12-10 11:47:37 +0900

    questionに関連する一連の回答を取得するためのロジックを実装しました。

---
+++ lib/models/services/answer_service.dart
@@ -0,0 +1,36 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/converters.dart';
+import 'package:qomalin_app/errors/question_error.dart';
+import 'package:qomalin_app/models/entities/answer.dart';
+import 'package:qomalin_app/providers/firestore.dart';
+
+class AnswerService {
+  Stream<List<Answer>> findByQuestion(String questionId) {
+    throw UnimplementedError();
+  }
+}
+
+class AnswerServiceFirestoreImpl implements AnswerService {
+  final Reader reader;
+  AnswerServiceFirestoreImpl(this.reader);
+  @override
+  Stream<List<Answer>> findByQuestion(String questionId) async* {
+    final question = await reader(FirestoreProviders.questionCollectionRefProvider())
+      .doc(questionId)
+      .get();
+
+    if(!question.exists) {
+      throw QuestionNotFoundException();
+    }
+
+    final answersStream = reader(FirestoreProviders.questionCollectionRefProvider())
+      .doc(questionId)
+      .collection('answers')
+      .withAnswerConverter()
+      .snapshots();
+
+    await for(final answer in answersStream) {
+      yield await Future.wait(answer.docs.map((e) async => await e.data().toEntity()).toList());
+    }
+  }
+}
