353a38e57aac7d93efa86ce426c6622c9c83178e
勇貴士 <isamu.takashi_momo@ezweb.ne.jp>
2021-12-10 09:55:17 +0900

    カードとログアウトボタンを配置した。

--- lib/ui/pages/account_page.dart
+++ lib/ui/pages/account_page.dart
@@ -8,13 +8,29 @@ class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("アカウント"),
+      ),
       body: Column(
         children: [
+          Card(
+            margin: const EdgeInsets.all(20),
+            child: Container(
+              width: double.infinity,
+              margin: const EdgeInsets.all(16),
+              height: 70,
+              child: const Text("こんにちは"),
+            ),
+          ),
           TextButton(
             onPressed: (){
               GoRouter.of(context).push("/me/profile/edit");
             },
             child: const Text("プロフィール編集", style: TextStyle(color: Colors.black))
+          ),
+          TextButton(
+            onPressed: (){},
+            child: const Text("ログアウト"),
           ),
         ],
       ),
