0d18d331ba3dbbcaa16be884bdfbc0799cd02ede
勇貴士 <isamu.takashi_momo@ezweb.ne.jp>
2021-12-21 16:20:26 +0900

    特定のユーザーが投稿した全質問を表示するListViewを作成した

---
+++ lib/ui/components/user_questions_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qomalin_app/models/entities/question.dart';
+import 'package:qomalin_app/providers/questions.dart';
+import 'package:qomalin_app/ui/components/question_card_list.dart';
+
+final _userQuestionsStreamProvider = StreamProvider.autoDispose((ref) {
+  return ref.read(QuestionProviders.questionServiceProvider()).questionsByUser(userId: "IJsmDX5VZxY3qnc8Qz2K3qRwmth2");
+});
+class UserQuestionList extends ConsumerWidget {
+  const UserQuestionList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<Question>> latestQuestionsStream = ref.watch(_userQuestionsStreamProvider);
+
+    return latestQuestionsStream.when(
+        data: (questions){
+          return QuestionCardList(
+              onQuestionSelectedListener: (q){
+                context.push('/questions/${q.id}/show');
+              },
+              onQuestionUserPressedListener: (u){},
+              questions: questions
+          );
+        },
+        error: (err, stack){
+          return const Text("取得失敗");
+        },
+        loading: (){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+}
+
+
+
